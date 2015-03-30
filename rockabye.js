11ae5229503967d272eb864df92821d7fc60814a
Leon Miller-Out <leon@singlebrook.com>
2015-03-30 19:45:56 -0400

    Simplify JS. Don't worry about adding style repeatedly.

--- rockabye.js
+++ rockabye.js
@@ -1,8 +1,4 @@
 // Called when the user clicks on the browser action.
-var rockabyeIsRockin = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
-  if (!rockabyeIsRockin) {
-    chrome.tabs.insertCSS({file: 'rockabye.css'});
-    rockabyeIsRockin = true;
-  }
+  chrome.tabs.insertCSS({file: 'rockabye.css'});
 });
